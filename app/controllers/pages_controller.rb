141ce742a15a5484264e62b42cb07a9f32a24351
andre ferrer <andrerferrer@hotmail.com>
2021-06-12 11:03:11 -0300

    refactor link to chatroom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @chatroom_id = Chatroom.first.id
+    @last_message_id = Message.last.id
   end
 end
