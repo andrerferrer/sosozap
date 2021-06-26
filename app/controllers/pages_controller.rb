e7b19e45409686d1e5b6f25572a5192955dd2994
andre ferrer <andrerferrer@hotmail.com>
2021-06-26 11:02:06 -0300

    Handle if we don't have a last message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
   def home
     @chatroom_id = Chatroom.first.id
-    @last_message_id = Message.last.id
+    @last_message_id = Message.last&.id
   end
 end
